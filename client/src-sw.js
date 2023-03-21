8dbfd26b6ff7c58d58e9f8a4c024095a1f6aa261
liliaungurean <lilicikau95@gmail.com>
2023-03-20 19:46:52 -0500

    src-sw

--- client/src-sw.js
+++ client/src-sw.js
@@ -27,4 +27,18 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(
+//Call back function, filter requests we want to cache
+  new StaleWhileRevalidate({
+    // Name of the cache storage.
+    cacheName: 'asset-cache',
+    plugins: [
+     new CacheableResponsePlugin({
+       statuses: [0, 200],
+     }),
+     new ExpirationPlugin({
+       maxAgeSeconds: 30 * 24 * 60 * 60,
+     }),
+   ],
+  })
+);
